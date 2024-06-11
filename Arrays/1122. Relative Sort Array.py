12ea6afdb251179807191999f0efa6c133a8c958
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2024-06-11 23:58:54 +0200

    Added description

--- Arrays/1122. Relative Sort Array.py
+++ Arrays/1122. Relative Sort Array.py
@@ -1,3 +1,7 @@
+# Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+# Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2.
+# Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
+
 from collections import Counter
 import heapq
 
@@ -5,10 +9,8 @@ arr1 = [28,6,22,8,44,17]
 arr2 = [22,28,8,6]
 
 def relativeSort(arr1, arr2):
-    m = len(arr2)
-    i = 0
     count = Counter(arr1)
-    res = [elem for elem in arr2 if elem in count for _ in range(count[elem])]
+    res = [elem for elem in arr2 for _ in range(count[elem])]
     heap = [elem for elem in arr1 if elem not in arr2]
     heapq.heapify(heap)
 
